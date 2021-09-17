88c7f3a97cbfa399751b525f3e3ce3f788ecd6f7
NeerajVodala <vodalaneeraj@gmail.com>
2021-09-18 03:06:57 +0530

    refactor: Logic

--- hypotenuseCalc.js
+++ hypotenuseCalc.js
@@ -3,9 +3,13 @@ const calcButton = document.querySelector(".btn-check");
 const result = document.querySelector(".result");
 
 calcButton.addEventListener("click", function calcHypotenuse() {
-    const sum = sumOfSquares(inputs[0].value, inputs[1].value);
-    const hypotenuse = Math.sqrt(sum);
-    result.innerText = "The length of Hypotenuse is " + hypotenuse.toFixed(2);
+    if (Number(inputs[0].value) > 0 && Number(inputs[1].value) > 0) {
+        const sum = sumOfSquares(inputs[0].value, inputs[1].value);
+        const hypotenuse = Math.sqrt(sum);
+        result.innerText = "The length of Hypotenuse is " + hypotenuse.toFixed(2);
+    } else {
+        result.innerText = "Values must be greater than 0"
+    }
 });
 
 function sumOfSquares() {
--- isTriangle.js
+++ isTriangle.js
@@ -5,10 +5,14 @@ const result = document.querySelector(".result");
 checkButton.addEventListener("click", function checkAngles() {
     const sum = sumOfAngles(inputAngles[0].value, inputAngles[1].value, inputAngles[2].value);
     if (inputAngles[0].value && inputAngles[1].value && inputAngles[2].value) {
-        if (sum === 180) {
-            result.innerText = "Yay! The angles form a Triangle";
+        if (inputAngles[0].value > 0 && inputAngles[1].value > 0 && inputAngles[2].value > 0) {
+            if (sum === 180) {
+                result.innerText = "Yay! The angles form a Triangle";
+            } else {
+                result.innerText = "oops! The angles don't form a Triangle";
+            }
         } else {
-            result.innerText = "oops! The angles don't form a Triangle";
+            result.innerText = "Angles must be greater than 0";
         }
     } else {
         result.innerText = "Please enter all the angles";
